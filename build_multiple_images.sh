7213acf4dc463e6bb7adab8a8b320d177e5a697e
Oguz Pastirmaci <oguzp@outlook.com>
2020-11-05 16:26:28 -0800

    Update build_multiple_images.sh

--- build_multiple_images.sh
+++ build_multiple_images.sh
@@ -10,7 +10,7 @@ IAD_IMAGE_ID=$(grep "ocid1.image.oc1.iad" packer.log | awk '{print $9}' | sed 's
 PHX_IMAGE_ID=$(grep "ocid1.image.oc1.phx" packer.log | awk '{print $9}' | sed 's/.$//')
 
 # Use the image IDs from the first step as the base image for the second step of image build. This will take the Oracle Linux 7.8 image that has Docker installed, pulls the container image and then creates a new image.
-#packer build -var "iad_base_image_id_from_first_step=$IAD_IMAGE_ID" -var "phx_base_image_id_from_first_step=$PHX_IMAGE_ID"  --var-file variables.json pull_container_and_build_image.json
+packer build -var "iad_base_image_id_from_first_step=$IAD_IMAGE_ID" -var "phx_base_image_id_from_first_step=$PHX_IMAGE_ID"  --var-file variables.json pull_container_and_build_image.json
 
 # Remove the log file
 rm packer.log
